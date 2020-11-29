256fb1c40453f0378bace5132652fb3bee587c70
Osiris <potterosiris@gmail.com>
2020-11-28 22:47:40 -0500

    require uv

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose")
+const uniqueValidator = require("mongoose-unique-validator")
+
+
 
 const Schema = mongoose.Schema
 
@@ -7,3 +10,5 @@ const userSchema = new Schema({
     email: {type:String, required: true},
     password: {type: String, required: true}
 })
+
+
