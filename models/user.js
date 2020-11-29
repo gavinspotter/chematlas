956640e6ce14904e40fd6ab891a50dfd9a29bb26
Osiris <potterosiris@gmail.com>
2020-11-28 22:48:28 -0500

    uv added to schema

--- models/user.js
+++ models/user.js
@@ -7,8 +7,10 @@ const Schema = mongoose.Schema
 
 const userSchema = new Schema({
     name: {type: String, required: true},
-    email: {type:String, required: true},
+    email: {type:String, required: true, unique:true},
     password: {type: String, required: true}
 })
+
+userSchema.plugin(uniqueValidator)
 
 
